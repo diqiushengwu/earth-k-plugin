a68b70c286567ad88288bc9668b41751062bac47
diqiushengwu <111220695+diqiushengwu@users.noreply.github.com>
2023-06-27 22:43:38 +0800

    Update MarryGroupFriends.js

--- apps/MarryGroupFriends.js
+++ apps/MarryGroupFriends.js
@@ -1,4 +1,4 @@
-import puppeteer from "../../lib/puppeteer/puppeteer.js";
+import puppeteer from "../../../lib/puppeteer/puppeteer.js";
 import fetch from "node-fetch";
 import fs from "fs";
 import {
